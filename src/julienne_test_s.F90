5cd08193c330222316a4d277bae873a94ac77abf
rouson <rouson@lbl.gov>
2024-09-22 23:29:51 +0000

    Deploying to gh-pages from @ BerkeleyLab/julienne@1eb98b8c5eb287ed27a94ddf8943656dafe8e8f4 🚀

--- src/julienne_test_s.F90
+++ src/julienne_test_s.F90
@@ -1,24 +1,22 @@
 ! Copyright (c) 2024, The Regents of the University of California and Sourcery Institute
 ! Terms of use are as specified in LICENSE.txt
+
+#include "language-support.F90"
+
 submodule(julienne_test_m) julienne_test_s
   use julienne_user_defined_collectives_m, only : co_all
   use julienne_command_line_m, only : command_line_t
   implicit none
 
-#if defined(__flang__)
-  #define NO_MULTI_IMAGE_SUPPORT
-#endif
-
 contains
 
   module procedure report
-#ifdef NO_MULTI_IMAGE_SUPPORT
+#if HAVE_MULTI_IMAGE_SUPPORT
+    associate(me => this_image())
+#else
     integer me
     me = 1
-#else
-    associate(me => this_image())
 #endif
-
 
       if (me==1) then
 
@@ -28,11 +26,12 @@ contains
             type(command_line_t) command_line
             test_description_substring = command_line%flag_value("--contains")
           end block
+          print *
           if (len(test_description_substring)==0) then
-            print*,"Running all tests."
-            print*,"(Add '-- --contains <string>' to run only tests with subjects or descriptions containing the specified string.)"
+            print '(a)',"Running all tests."
+            print '(a)',"(Add '-- --contains <string>' to run only tests with subjects or descriptions containing the specified string.)"
           else
-            print *,"Running only tests with subjects or descriptions containing '", test_description_substring,"'."
+            print '(*(a))',"Running only tests with subjects or descriptions containing '", test_description_substring,"'."
           end if
         end if first_report
 
@@ -40,7 +39,7 @@ contains
 
       end if
 
-#ifndef NO_MULTI_IMAGE_SUPPORT
+#if HAVE_MULTI_IMAGE_SUPPORT
       call co_broadcast(test_description_substring, source_image=1)
 #endif
       
@@ -52,23 +51,21 @@ contains
             block
               integer i
               do i=1,num_tests
-                if (me==1) print *,"   ",test_results(i)%characterize()
+                if (me==1) print '(3x,a)', test_results(i)%characterize()
               end do
             end block
           end if
           block 
             logical, allocatable :: passing_tests(:)
             passing_tests = test_results%passed()
-#ifndef NO_MULTI_IMAGE_SUPPORT
             call co_all(passing_tests)
-#endif
             associate(num_passes => count(passing_tests))
               if (me==1) print '(a,2(i0,a))'," ",num_passes," of ", num_tests," tests pass."
               passes = passes + num_passes
             end associate
           end block
         end associate
-#ifndef NO_MULTI_IMAGE_SUPPORT
+#if HAVE_MULTI_IMAGE_SUPPORT
       end associate
 #endif
 
@@ -83,7 +80,7 @@ contains
           tests = tests + num_tests
           if (me==1) then
             do i=1,num_tests
-              if (me==1) print *,"   ",test_results(i)%characterize()
+              if (me==1) print '(3x,a)', test_results(i)%characterize()
             end do
           end if
           passing_tests = test_results%passed()
