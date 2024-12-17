34db8b11f0bcf3b23f21c0b5e4d999c51adf6e42
Damian Rouson <rouson@lbl.gov>
2024-12-16 22:22:42 -0800

    test(command_line_t): improve test robustness
    This commit replaces the previously fragile unit tests for the
    command_line_t type with tests that no longer require launching an
    fpm subprocess via execute_command_line and checking that child
    process's exit status.  The test main program also now prints
    instructions for how to run the command_line_t tests if the flags
    for running those tests is not detected.

--- test/command_line_test.F90
+++ test/command_line_test.F90
@@ -35,7 +35,7 @@ contains
     test_descriptions = [ & 
       test_description_t(string_t("returning the value passed after a command-line flag"), check_flag_value), &
       test_description_t(string_t("returning an empty string when a flag value is missing"), handle_missing_flag_value), &
-      test_description_t(string_t("detecting a present command-line argument"), check_command_line_argument) &
+      test_description_t(string_t("detecting a present command-line argument"), check_argument_present) &
     ]   
 #else
     ! Work around missing Fortran 2008 feature: associating a procedure actual argument with a procedure pointer dummy argument:
@@ -57,38 +57,20 @@ contains
 
   function check_flag_value() result(test_passes)
     logical test_passes
-    integer exit_status, command_status
-    character(len=132) command_message
-
-    call execute_command_line( &
-      command = "fpm run --example get-flag-value -- --input-file some_file_name", &
-      wait = .true., exitstat = exit_status, cmdstat = command_status, cmdmsg = command_message &
-    )   
-    test_passes = exit_status == 0 
+    type(command_line_t) command_line
+    test_passes = command_line%flag_value("--test") == "command_line_t"
   end function
 
   function handle_missing_flag_value() result(test_passes)
     logical test_passes
-    integer exit_status, command_status
-    character(len=132) command_message
-
-    call execute_command_line( &
-      command = "fpm run --example handle-missing-flag -- --empty-flag", &
-      wait = .true., exitstat = exit_status, cmdstat = command_status, cmdmsg = command_message &
-    )   
-    test_passes = exit_status == 0 
+    type(command_line_t) command_line
+    test_passes = command_line%flag_value("--type") == ""
   end function
 
-  function check_command_line_argument() result(test_passes)
+  function check_argument_present() result(test_passes)
     logical test_passes
-    integer exit_status, command_status
-    character(len=132) command_message
-
-    call execute_command_line( &
-      command = "fpm run --example check-command-line-argument -- --some-argument", &
-      wait = .true., exitstat = exit_status, cmdstat = command_status, cmdmsg = command_message &
-    )
-    test_passes = exit_status == 0
+    type(command_line_t) command_line
+    test_passes = command_line%argument_present(["--type"])
   end function
 
 end module command_line_test_m
