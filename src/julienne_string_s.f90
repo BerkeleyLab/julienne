6ebf42a07ed7b8fb0007ab242aa1db5f0a2bdff3
rouson <rouson@lbl.gov>
2025-02-05 04:06:33 +0000

    Deploying to gh-pages from @ BerkeleyLab/julienne@a2f36032a486eace3b7c91b4a7ba04ec68d154d4 🚀

--- src/julienne_string_s.f90
+++ src/julienne_string_s.f90
@@ -230,7 +230,11 @@ contains
     end associate
   end procedure
 
-  module procedure get_string_t_array
+  module procedure get_string_t_array_with_string_t_key
+    value_ = self%get_string_t_array_with_character_key(key%string(), mold)
+  end procedure
+
+  module procedure get_string_t_array_with_character_key
 
     character(len=:), allocatable :: raw_line
     integer i, comma, opening_quotes, closing_quotes
@@ -242,7 +246,7 @@ contains
     associate(colon => index(raw_line, ':'))
       associate(opening_bracket => colon + index(raw_line(colon+1:), '['))
         associate(closing_bracket => opening_bracket + index(raw_line(opening_bracket+1:), ']'))
-          associate(commas => count("," == [(raw_line(i:i), i = opening_bracket+1, closing_bracket-1)]))
+          associate(commas => count([(raw_line(i:i)==",", i = opening_bracket+1, closing_bracket-1)]))
             allocate(value_(commas+1))
             opening_quotes = opening_bracket + index(raw_line(opening_bracket+1:), '"')
             closing_quotes = opening_quotes + index(raw_line(opening_quotes+1:), '"')
