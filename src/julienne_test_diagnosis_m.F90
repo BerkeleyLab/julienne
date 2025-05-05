828dbc6ae65121725bc0835c399bb6b40f413864
rouson <rouson@lbl.gov>
2025-05-05 20:50:07 +0000

    Deploying to gh-pages from @ BerkeleyLab/julienne@45d2881efb8cec54e5e058702b7c0c083e156b3c 🚀

--- src/julienne_test_diagnosis_m.F90
+++ src/julienne_test_diagnosis_m.F90
@@ -1,5 +1,8 @@
 ! Copyright (c) 2024-2025, The Regents of the University of California and Sourcery Institute
 ! Terms of use are as specified in LICENSE.txt
+
+#include "language-support.F90"
+
 module julienne_test_diagnosis_m
   !! Define an abstraction for describing test outcomes and diagnostic information
   use julienne_string_m, only : string_t
@@ -7,6 +10,15 @@ module julienne_test_diagnosis_m
 
   private
   public :: test_diagnosis_t
+  public :: operator(.all.)
+  public :: operator(.and.)
+  public :: operator(.approximates.)
+  public :: operator(.within.)
+  public :: operator(.equalsExpected.)
+  public :: operator(.lessThan.)
+  public :: operator(.lessThanOrEqualTo.)
+  public :: operator(.greaterThan.)
+  public :: operator(.greaterThanOrEqualTo.)
 
   type test_diagnosis_t
     !! Encapsulate test outcome and diagnostic information
@@ -17,6 +29,159 @@ module julienne_test_diagnosis_m
     procedure test_passed
     procedure diagnostics_string
   end type
+
+  integer, parameter :: default_real = kind(1.), double_precision = kind(1D0)
+
+#if HAVE_DERIVED_TYPE_KIND_PARAMETERS
+  type operands_t(k)
+    integer, kind :: k = default_real
+    real(k) actual, expected 
+  end type
+#else
+  type operands_t
+    real actual, expected 
+  end type
+
+  type double_precision_operands_t
+    double precision actual, expected 
+  end type
+#endif
+
+  interface operator(.all.)
+     
+    pure module function aggregate_diagnosis(diagnoses) result(diagnosis)
+      implicit none
+      type(test_diagnosis_t), intent(in) :: diagnoses(..)
+      type(test_diagnosis_t) diagnosis
+    end function
+
+  end interface
+
+  interface operator(.and.)
+     
+    elemental module function and(lhs, rhs) result(diagnosis)
+      implicit none
+      type(test_diagnosis_t), intent(in) :: lhs, rhs
+      type(test_diagnosis_t) diagnosis
+    end function
+
+  end interface
+
+  interface operator(.approximates.)
+
+    elemental module function approximates_real(actual, expected) result(operands)
+      implicit none
+      real, intent(in) :: actual, expected
+      type(operands_t) operands
+    end function
+
+    elemental module function approximates_double_precision(actual, expected) result(operands)
+      implicit none
+      double precision, intent(in) :: actual, expected
+#if HAVE_DERIVED_TYPE_KIND_PARAMETERS
+      type(operands_t(double_precision)) operands
+#else
+      type(double_precision_operands_t) operands
+#endif
+    end function
+
+  end interface
+
+  interface operator(.equalsExpected.)
+
+    elemental module function equals_expected_integer(actual, expected) result(test_diagnosis)
+      implicit none
+      integer, intent(in) :: actual, expected
+      type(test_diagnosis_t) test_diagnosis
+    end function
+
+  end interface
+
+  interface operator(.lessThan.)
+
+    elemental module function less_than_real(actual, expected_ceiling) result(test_diagnosis)
+      implicit none
+      real, intent(in) :: actual, expected_ceiling
+      type(test_diagnosis_t) test_diagnosis
+    end function
+
+    elemental module function less_than_double(actual, expected_ceiling) result(test_diagnosis)
+      implicit none
+      double precision, intent(in) :: actual, expected_ceiling
+      type(test_diagnosis_t) test_diagnosis
+    end function
+
+    elemental module function less_than_integer(actual, expected_ceiling) result(test_diagnosis)
+      implicit none
+      integer, intent(in) :: actual, expected_ceiling
+      type(test_diagnosis_t) test_diagnosis
+    end function
+
+  end interface
+
+  interface operator(.lessThanOrEqualTo.)
+
+    elemental module function less_than_or_equal_to_integer(actual, expected_max) result(test_diagnosis)
+      implicit none
+      integer, intent(in) :: actual, expected_max
+      type(test_diagnosis_t) test_diagnosis
+    end function
+
+  end interface
+
+  interface operator(.greaterThanOrEqualTo.)
+
+    elemental module function greater_than_or_equal_to_integer(actual, expected_min) result(test_diagnosis)
+      implicit none
+      integer, intent(in) :: actual, expected_min
+      type(test_diagnosis_t) test_diagnosis
+    end function
+
+  end interface
+
+  interface operator(.greaterThan.)
+
+    elemental module function greater_than_real(actual, expected_floor) result(test_diagnosis)
+      implicit none
+      real, intent(in) :: actual, expected_floor
+      type(test_diagnosis_t) test_diagnosis
+    end function
+
+    elemental module function greater_than_double(actual, expected_floor) result(test_diagnosis)
+      implicit none
+      double precision, intent(in) :: actual, expected_floor
+      type(test_diagnosis_t) test_diagnosis
+    end function
+
+    elemental module function greater_than_integer(actual, expected_floor) result(test_diagnosis)
+      implicit none
+      integer, intent(in) :: actual, expected_floor
+      type(test_diagnosis_t) test_diagnosis
+    end function
+
+  end interface
+
+  interface operator(.within.)
+
+    elemental module function within_real(operands, tolerance) result(test_diagnosis)
+      implicit none
+      type(operands_t), intent(in) :: operands
+      real, intent(in) :: tolerance
+      type(test_diagnosis_t) test_diagnosis
+    end function
+   
+    elemental module function within_double_precision(operands, tolerance) result(test_diagnosis)
+      implicit none
+#if HAVE_DERIVED_TYPE_KIND_PARAMETERS
+      type(operands_t(double_precision)), intent(in) :: operands
+#else
+      type(double_precision_operands_t), intent(in) :: operands
+#endif
+      double precision, intent(in) :: tolerance
+      type(test_diagnosis_t) test_diagnosis
+    end function
+   
+  end interface
 
   interface test_diagnosis_t
 
