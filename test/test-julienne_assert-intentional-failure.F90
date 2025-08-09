b0c034bbe14502da661689e0060273f5a9985084
Damian Rouson <rouson@lbl.gov>
2025-08-09 07:56:07 -0700

    fix(intentional-fail): mv 'implicit none'

--- test/test-julienne_assert-intentional-failure.F90
+++ test/test-julienne_assert-intentional-failure.F90
@@ -6,13 +6,11 @@
 program test_julienne_assert_intentional_failure
   !! Conditionally test an assertion that is hardwired to fail.
   use julienne_m, only : call_julienne_assert_, command_line_t, operator(.equalsExpected.)
+  implicit none
 
   associate(command_line => command_line_t())
-
     if (.not. command_line%argument_present([character(len=len("--help"))::"--help","-h"])) then
-
 #ifdef RUN_FALSE_ASSERTIONS
-      implicit none
       print '(a)', new_line('') // 'Test julienne_assert intentional failure: ' // new_line('')
       call_julienne_assert(1 .equalsExpected. 2)
 #else
@@ -21,9 +19,7 @@ program test_julienne_assert_intentional_failure
       print '(a)', 'Add the following to your fpm command to test assertion failure: --flag "-DASSERTIONS -DRUN_FALSE_ASSERTIONS"'
       print *
 #endif
-
     end if
-
   end associate
 
 end program
