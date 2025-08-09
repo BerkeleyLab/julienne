a738cacbf756b43bfcd6d59f5956279173c3c6fe
Damian Rouson <rouson@lbl.gov>
2025-08-09 07:40:25 -0700

    feat(test):don't run intentional fail if --help/-h

--- test/test-julienne_assert-intentional-failure.F90
+++ test/test-julienne_assert-intentional-failure.F90
@@ -5,22 +5,26 @@
 
 program test_julienne_assert_intentional_failure
   !! Conditionally test an assertion that is hardwired to fail.
+  use julienne_m, only : call_julienne_assert_, command_line_t, operator(.equalsExpected.)
+
+  associate(command_line => command_line_t())
+
+    if (.not. command_line%argument_present([character(len=len("--help"))::"--help","-h"])) then
 
 #ifdef RUN_FALSE_ASSERTIONS
-
-  use julienne_m, only : operator(.equalsExpected.), call_julienne_assert_
-  implicit none
-  print '(a)', new_line('') // 'Test julienne_assert intentional failure: ' // new_line('')
-  call_julienne_assert(1 .equalsExpected. 2)
-
+      implicit none
+      print '(a)', new_line('') // 'Test julienne_assert intentional failure: ' // new_line('')
+      call_julienne_assert(1 .equalsExpected. 2)
 #else
-
-  print *
-  print '(a)', 'Skipping the test in ' // __FILE__ // '.'
-  print '(a)', 'Add the following to your fpm command to test assertion failure: --flag "-DASSERTIONS -DRUN_FALSE_ASSERTIONS"'
-  print *
-
+      print *
+      print '(a)', 'Skipping the test in ' // __FILE__ // '.'
+      print '(a)', 'Add the following to your fpm command to test assertion failure: --flag "-DASSERTIONS -DRUN_FALSE_ASSERTIONS"'
+      print *
 #endif
+
+    end if
+
+  end associate
 
 end program
 
