c2f951c0ecb73a91e67418e8d5bbda7d459b6b2b
Damian Rouson <rouson@lbl.gov>
2025-08-09 07:39:33 -0700

    refac(test_harness): mv help I/O to harness report

--- src/julienne/julienne_test_harness_s.F90
+++ src/julienne/julienne_test_harness_s.F90
@@ -2,6 +2,7 @@
 ! Terms of use are as specified in LICENSE.txt
 
 submodule(julienne_test_harness_m) julienne_test_harness_s
+  use julienne_command_line_m, only : command_line_t
   implicit none
 
 contains
@@ -11,10 +12,36 @@ contains
     end procedure
 
     module procedure report
-      integer i
-      do i = 1, size(self%test_fixture_)
-        call self%test_fixture_(i)%report(passes, tests, skips)
-      end do
+
+      call print_usage_info_and_stop_if_requested
+
+      block
+        integer i
+        do i = 1, size(self%test_fixture_)
+          call self%test_fixture_(i)%report(passes, tests, skips)
+        end do
+      end block
+
     end procedure
+
+    subroutine print_usage_info_and_stop_if_requested
+
+      associate(command_line => command_line_t())
+        block
+          character(len=*), parameter :: usage = &
+            new_line('') // new_line('') // &
+            'Usage: fpm test -- [--help] | [--contains <substring>]' // &
+            new_line('') // new_line('') // &
+            'where square brackets ([]) denote optional arguments, a pipe (|) separates alternative arguments,' // new_line('') // &
+            'angular brackets (<>) denote a user-provided value, and passing a substring limits execution to' // new_line('') // &
+            'the tests with test subjects or test descriptions containing the user-specified substring.' // new_line('')
+
+          if (command_line%argument_present([character(len=len("--help"))::"--help","-h"])) stop usage
+        end block
+      end associate
+
+      print "(a)", new_line("") // "Append '-- --help' or '-- -h' to your `fpm test` command to display usage information."
+
+    end subroutine
 
 end submodule julienne_test_harness_s
