26f4647f7f53d8667539c239637e0e361bfae319
Damian Rouson <rouson@lbl.gov>
2025-08-23 11:20:51 -0700

    test: match test file name to test program name

--- test/logical_assertion_failure_test.F90
+++ test/logical_assertion_failure_test.F90
@@ -3,16 +3,18 @@
 
 #include "julienne-assert-macros.h"
 
-program test_julienne_assert_intentional_failure
+program logical_assertion_failure_test
   !! Conditionally test an assertion that is hardwired to fail.
   use julienne_m, only : call_julienne_assert_, command_line_t, operator(.equalsExpected.)
   implicit none
+  integer, allocatable :: array(:)
 
   associate(command_line => command_line_t())
     if (.not. command_line%argument_present([character(len=len("--help"))::"--help","-h"])) then
 #ifdef RUN_FALSE_ASSERTIONS
-      print '(a)', new_line('') // 'Test the intentional failure of an idiomatic assertion: ' // new_line('')
-      call_julienne_assert(1 .equalsExpected. 2)
+      print '(a)', new_line('') // 'Test the intentional failure of a logical assertion: ' // new_line('')
+      if (allocated(array)) deallocate(array)
+      call_julienne_assert(allocated(array))
 #else
       print *
       print '(a)', 'Skipping the test in ' // __FILE__ // '.'
@@ -23,4 +25,3 @@ program test_julienne_assert_intentional_failure
   end associate
 
 end program
-
