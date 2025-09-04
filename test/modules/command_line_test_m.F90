03e6517512b04923b089d97b15956364d850657d
Damian Rouson <rouson@lbl.gov>
2025-09-03 22:31:27 -0700

    fix(command_line_test): rm extra comma

--- test/modules/command_line_test_m.F90
+++ test/modules/command_line_test_m.F90
@@ -71,7 +71,7 @@ contains
 #else
       block
       procedure(diagnosis_function_i), pointer :: &
-        ,check_flag_value_ptr         => check_flag_value &
+         check_flag_value_ptr         => check_flag_value &
         ,check_flag_value_missing_ptr => check_flag_value_missing &
         ,check_flag_missing_ptr       => check_flag_missing &
         ,check_argument_missing_ptr   => check_argument_missing &
