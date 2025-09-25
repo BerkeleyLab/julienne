5d2aeb833a248b754df20870f6a5ed36a0f57365
Damian Rouson <rouson@lbl.gov>
2025-09-25 13:28:47 -0500

    chore(logical_assert_fail_test):rm unused operator

--- test/logical_assertion_failure_test.F90
+++ test/logical_assertion_failure_test.F90
@@ -6,7 +6,7 @@
 
 program logical_assertion_failure_test
   !! Conditionally test an assertion that is hardwired to fail.
-  use julienne_m, only : call_julienne_assert_, command_line_t, operator(.equalsExpected.)
+  use julienne_m, only : call_julienne_assert_, command_line_t
   implicit none
   integer, allocatable :: array(:)
 
