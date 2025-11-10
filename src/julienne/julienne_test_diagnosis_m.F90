d12001f61eaec041db5921b56f24b11e234f383d
Damian Rouson <rouson@lbl.gov>
2025-11-09 21:48:22 -0800

    fix(nag): work around compiler issue
    An elemental function with no arguments caused an internal compiler
    error.  Replacing `elemental` with `pure` eliminates the error.

--- src/julienne/julienne_test_diagnosis_m.F90
+++ src/julienne/julienne_test_diagnosis_m.F90
@@ -544,7 +544,7 @@ module julienne_test_diagnosis_m
       logical, intent(in) :: rhs
     end subroutine
 
-    elemental module function passing_test() result(test_diagnosis)
+    pure module function passing_test() result(test_diagnosis)
       !! Construct a passing test diagnosis with a zero-length diagnostics string
       implicit none
       type(test_diagnosis_t) test_diagnosis
