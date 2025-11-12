c70c3e2a9ea12039aadc29ffd16cc3e64be33d26
rouson <rouson@lbl.gov>
2025-11-12 19:28:12 +0000

    Deploying to gh-pages from @ BerkeleyLab/julienne@a13db18b726ce31575d8c3b6ca117a42cc45e52d 🚀

--- src/julienne_test_harness_s.F90
+++ src/julienne_test_harness_s.F90
@@ -12,7 +12,11 @@ submodule(julienne_test_harness_m) julienne_test_harness_s
 contains
 
     module procedure component_constructor
-      allocate(test_harness%test_fixture_, source = test_fixtures)
+#ifdef NAGFOR
+      test_harness%test_fixture_ = test_fixtures ! avoid a nagfor internal compiler error
+#else
+      allocate(test_harness%test_fixture_, source = test_fixtures) ! eliminates a harmless gfortran warning
+#endif
     end procedure
 
     module procedure report_results
